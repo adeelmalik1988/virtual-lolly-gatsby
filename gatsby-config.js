8fe409989976b45105d53d8d1af88a29ccf83d1c
Adeel Malik <adeelmalik1988@gmail.com>
2021-02-09 22:05:13 +0500

    yarn install for deployment on Netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,26 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
+// const { createHttpLink } = require("@apollo/client")
+
+// /**
+//  * Configure your Gatsby site with this file.
+//  *
+//  * See: https://www.gatsbyjs.com/docs/gatsby-config/
+//  */
+// require("dotenv").config({
+//   path: `.env.${process.env.FAUNA}`
+// })
+
+
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    // {
+    //   resolve: "gatsby-source-graphql",
+    //   options: {
+    //     secret: process.env.FAUNA,
+
+
+    //   }
+    // }
+  ],
 }
